ff975cfd106075782f340d4727665d82768f99f4
Erodos93 <horcicka.student@gmail.com>
2022-06-02 16:51:04 +0200

    Starting point for my project Designo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Layout/Header';
+import Home from './components/Layout/Home';
+import Footer from './components/Layout/Footer';
+
+
+const App =()=> {
+  return (<div>
+
+    <Header/>
+    <Home/>
+    <Footer/>
+
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Home = ()=>{
+
+    return (<section className="grid__box">
+
+
+    </section>)
+}
+export default Home;
